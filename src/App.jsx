a0a7a3fd7090febc9a4ace02d146ea36c04f5871
Aroha-Palacios-del-Pilar <aroha.palacios@gmail.com>
2024-11-18 18:32:01 +0100

    kanban to-do list first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import KanbanBoard from './components/KanbanBoard'
+
+function App() {
+
+
+  return (
+    <div className='App'>
+    <KanbanBoard/>
+
+    </div>
+  )
+}
+
+export default App
